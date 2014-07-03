fce864917094a032e379f4345592d57a7e382f0f
daya <daya0576@gmail.com>
2014-07-03 20:48:48 +0800

    edit shell， to control manual file input

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import os
 
 file_dir = "input/vm_flow_matrix/"
-file_list = os.listdir(file_dir)
-
+#file_list = os.listdir(file_dir)
+file_list = ["1Partition@15.data"]
 
 for filename in file_list:
     print filename + "\ting....."
