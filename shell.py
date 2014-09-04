82af4f16c05a83a8bbcf55ed5e2b95621071d61a
daya <daya0576@gmail.com>
2014-09-04 09:17:16 +0800

    add method

--- shell.py
+++ shell.py
@@ -13,7 +13,7 @@ for filename in file_list:
         cmd1 = "h:/PYTHON26/python.exe 1_MC_BT_algorithm.py " + file_dir + filename + method
         #  + " > test/" + filename +"_test1.txt" 
         os.system(cmd1)
-        
+         
         cmd2 = "h:/PYTHON26/python.exe 2_BF_algorithm.py " + file_dir + filename
         # + " > test/" + filename +"_test2.txt"
         os.system(cmd2)
